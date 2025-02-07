e174930c808dd26d78d684f28c1fb220c6bb4960
Natalí <natali.cmr21@gmail.com>
2025-02-07 14:24:15 -0300

    First Commit

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+#madlibs game
+
+noun = input("Write a noun: ")
+adj_1 = input("Write a adjective: ")
+adj_2 = input("Write another adjective: ")
+verb = input("Write a verb: ")
+
+print()
+print("I went to the zoo.")
+print(f"There I saw a {noun},")
+print(f"It was a {adj_1} and {adj_2} {noun}.")
+print(f"I wish I could {verb} it.")
